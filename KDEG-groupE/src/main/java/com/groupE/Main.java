f885d7d4188cac5f57b65a45fc089bc3464a96e1
amogh5 <44971559+amogh5@users.noreply.github.com>
2018-11-22 16:07:28 +0000

    Update Main.java
    Call for GUI Initialization

--- KDEG-groupE/src/main/java/com/groupE/Main.java
+++ KDEG-groupE/src/main/java/com/groupE/Main.java
@@ -34,7 +34,7 @@ public class Main {
 	public static void main(String[] args) throws Exception {
 		Files.copy( URI.create(firstDatasetOnline).toURL().openStream(), Paths.get(firstDatasetLocal),StandardCopyOption.REPLACE_EXISTING);
 		createOntology();
-		
+		QueryGUI.initializeAppGUI();
 		/*---------Amogh's UI will go down here-----------*/
 	}
 	private static void createOntology() throws Exception {
